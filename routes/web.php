059c51e1abf45de13bc15015afe1a39c1d08805b
shailesh ladumor <shaileshmladumor@gmail.com>
2021-03-24 08:45:06 +0530

    feat: dompdf demo add

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Dompdf\Dompdf;
 
 Route::get('/', function () {
-    return view('welcome');
+    $dompdf = new Dompdf();
+    $dompdf->loadHtml(view('print_pdf'));
+
+// (Optional) Setup the paper size and orientation
+    $dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+    $dompdf->render();
+
+// Output the generated PDF to Browser
+    $dompdf->stream('demo.pdf');
 });
 
 Route::get('tickets', [TicketController::class, 'index']);
